d7695b6fb2995af3c0fec9901dbad5dd69afd081
TURKZEN <batuhanthd@gmail.com>
2022-01-16 19:50:21 +0300

    Firs commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Fake Person Generator 
+# Sahte Kimlik Oluşturucu
+
+
+![project_screenshot](https://i.hizliresim.com/euum2pc.png)
+
+
+## Installation
+
+Öncelikle Faker kütüphanesini indirin.
+Ardından projeyi klonlayın.
+
+```bash
+ pip install Faker
+ git clone https://github.com/TURKZEN/FakePersonGenerator.git
+
+```
+
+## Usage
+
+Projeyi cloneladıktan sonra Visual Studio Code programında açınız.
+
+Linux için:
+```linux
+cd FakePersonGenerator
+python fakepersongenerator.py
+```
+
+## Contributing
+Pull requestler kabul edilir. Büyük değişiklikler için, lütfen önce neyi değiştirmek istediğinizi tartışmak için bir konu açınız.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
+
---
+++ fakepersongenerator.py
@@ -0,0 +1,324 @@
+import os
+os.system('cls' if os.name=='nt' else 'clear')
+from faker import Faker
+print(
+"""
+  ______    _        _____                           _____                           _             
+ |  ____|  | |      |  __ \                         / ____|                         | |            
+ | |__ __ _| | _____| |__) |__ _ __ ___  ___  _ __ | |  __  ___ _ __   ___ _ __ __ _| |_ ___  _ __ 
+ |  __/ _` | |/ / _ \  ___/ _ \ '__/ __|/ _ \| '_ \| | |_ |/ _ \ '_ \ / _ \ '__/ _` | __/ _ \| '__|
+ | | | (_| |   <  __/ |  |  __/ |  \__ \ (_) | | | | |__| |  __/ | | |  __/ | | (_| | || (_) | |   
+ |_|  \__,_|_|\_\___|_|   \___|_|  |___/\___/|_| |_|\_____|\___|_| |_|\___|_|  \__,_|\__\___/|_|   
+                                                                            
+        Sahte Kimlik Oluşturucu - Fake Person Generator 
+                Coder : Batuhan Türkarslan 
+
+Choose a language (Bir Dil Seç) :
+ [1]English
+ [2]Türkçe 
+
+ [q]Exit (Çıkış)
+"""
+)
+lan = input("Your choose (Seçimin) : ")
+
+if lan =="q":
+    print("""
+    See you again...
+    Tekrar görüşeceğiz...
+    """)
+# English - Start
+elif lan == "1":
+    while True:
+        cho=input("""
+        What language should the fake information be created in?
+        [1] English
+        [2] Turkish
+
+        Your choose :  """)
+        if cho=="1":
+            fake = Faker(['en_US'])
+            print(
+                """
+                [1] Fake Name
+                [2] Fake Adress
+                [3]
+                [4]
+                [5]
+                
+                Exit[q]
+                """)
+            choose = input("Your choose : ")
+
+            if choose == "q":
+                print("\nwe'll meet Again...")
+                break
+            elif choose == "1":
+                print("""
+-----------------------------------------------------
+Fake name created : {0}             
+-----------------------------------------------------
+                """.format(fake.name()))
+                con = input("Do you want to continue (y/n) : ")
+                if con == "n":
+                    print("\nwe'll meet Again...")
+                    break
+                elif con == "y":
+                    continue
+                else:
+                    print("""
+                    
+                    Please make the right choice.
+                    (y/n) ---> (yes/no)
+
+                    """)
+                    break
+            elif choose == "2":
+                print("""
+-----------------------------------------------------
+Fake adress created : 
+
+{0}             
+-----------------------------------------------------
+                """.format(fake.address()))
+                
+                con = input("Do you want to continue (y/n) : ")
+                if con == "n":
+                    print("\nwe'll meet Again...")
+                    break
+                elif con == "y":
+                    continue
+                else:
+                    print("""
+                    
+                    Please make the right choice.
+                    (y/n) ---> (yes/no)
+
+                    """)
+                    break
+        elif cho =="2":
+            fake = Faker(['tr_TR'])
+            print(
+                """
+                [1] Fake Name
+                [2] Fake Adress
+                [3]
+                [4]
+                [5]
+                
+                Exit[q]
+                """)
+            choose = input("Your choose : ")
+
+            if choose == "q":
+                print("\nwe'll meet Again...")
+                break
+            elif choose == "1":
+                print("""
+-----------------------------------------------------
+Fake name created : {0}             
+-----------------------------------------------------
+                """.format(fake.name()))
+                con = input("Do you want to continue (y/n) : ")
+                if con == "n":
+                    print("\nwe'll meet Again...")
+                    break
+                elif con == "y":
+                    continue
+                else:
+                    print("""
+                    
+                    Please make the right choice.
+                    (y/n) ---> (yes/no)
+
+                    """)
+                    break
+            elif choose == "2":
+                print("""
+-----------------------------------------------------
+Fake adress created : 
+
+{0}             
+-----------------------------------------------------
+                """.format(fake.address()))
+                
+                con = input("Do you want to continue (y/n) : ")
+                if con == "n":
+                    print("\nwe'll meet Again...")
+                    break
+                elif con == "y":
+                    continue
+                else:
+                    print("""
+                    
+                    Please make the right choice.
+                    (y/n) ---> (yes/no)
+
+                    """)
+                    break
+        else:
+            print("""
+
+        Please make the right choice
+
+        [1] English
+        [2] Turkish
+            
+            """)
+            break
+#English - End
+
+#Turkish - Start
+elif lan == "2":
+    while True:
+        cho=input("""
+        Sahte bilgiler hangi dilde oluşturulsun ? 
+        [1] İngilizce
+        [2] Türkçe
+
+        Seçimin :  """)
+        if cho=="1":
+            fake = Faker()
+            print(
+                """
+                [1] Sahte İsim
+                [2] Sahte Adres
+                [3]
+                [4]
+                [5]
+                
+                Çıkış[q]
+                """)
+            choose = input("Seçimin : ")
+
+            if choose == "q":
+                print("\nTekrar görüşeceğiz...")
+                break
+            elif choose == "1":
+                print("""
+-----------------------------------------------------
+Sahte isim oluşturuldu : {0}             
+-----------------------------------------------------
+                """.format(fake.name()))
+                con = input("Devam etmek istiyormusun (e/h) : ")
+                if con == "h":
+                    print("\nTekrar görüşeceğiz...")
+                    break
+                elif con == "e":
+                    continue
+                else:
+                    print("""
+                    
+                    Lütfen doğru seçim yapınız
+                    (e/h) ---> (evet/hayır)
+
+                    """)
+                    break
+            elif choose == "2":
+                print("""
+-----------------------------------------------------
+Sahte adres oluşturuldu : 
+
+{0}             
+-----------------------------------------------------
+                """.format(fake.address()))
+                
+                con = input("Devam etmek istiyormusun (e/h) : ")
+                if con == "h":
+                    print("\nTekrar görüşeceğiz...")
+                    break
+                elif con == "e":
+                    continue
+                else:
+                    print("""
+                    
+                    Lütfen doğru seçim yapınız.
+                    (e/h) ---> (evet/hayır)
+
+                    """)
+                    break
+        elif cho =="2":
+            fake = Faker(['tr_TR'])
+            print(
+                """
+                [1] Sahte İsim
+                [2] Sahte Adres
+                [3]
+                [4]
+                [5]
+                
+                Exit[q]
+                """)
+            choose = input("Seçimin : ")
+
+            if choose == "q":
+                print("\nTekrar görüşeceğiz...")
+                break
+            elif choose == "1":
+                print("""
+-----------------------------------------------------
+Sahte isim oluşturuldu : {0}             
+-----------------------------------------------------
+                """.format(fake.name()))
+                con = input("Devam etmek istiyormusun (e/h) : ")
+                if con == "h":
+                    print("\nTekrar görüşeceğiz...")
+                    break
+                elif con == "e":
+                    continue
+                else:
+                    print("""
+                    
+                    Lütfen doğru seçim yapınız
+                    (e/h) ---> (evet/hayır)
+
+                    """)
+                    break
+            elif choose == "2":
+                print("""
+-----------------------------------------------------
+Sahte adres oluşturuldu : 
+
+{0}             
+-----------------------------------------------------
+                """.format(fake.address()))
+                
+                con = input("Devam etmek istiyormusunuz (e/h) : ")
+                if con == "h":
+                    print("\nTekrar görüşeceğiz...")
+                    break
+                elif con == "e":
+                    continue
+                else:
+                    print("""
+                    
+                    Lütfen doğru seçimi yapınız
+                    (e/h) ---> (evet/hayır)
+
+                    """)
+                    break
+        else:
+            print("""
+
+        Lütfen doğru seçimi yapınız
+
+        [1] İngilizce
+        [2] Türkçe
+            
+            """)
+            break
+#Turkish - End
+
+else:
+    print("""
+
+    Please choose a language
+
+    Lütfen bir dil seç
+
+    English : [EN]
+    Türkçe  : [TR]
+    """)
+
+    
+    
